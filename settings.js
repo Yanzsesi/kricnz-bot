c4441de1fee6117ea1a89bcb57cd69dc9b1401db
Yanzsesi <94530521+Yanzsesi@users.noreply.github.com>
2022-10-04 20:23:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Z4K" //namabot kalian
-global.ownername= "Z4K STORE" //nama kalian
+global.botname = "Rimuru" //namabot kalian
+global.ownername= "Yanz" //nama kalian
 global.myweb ="https://api.krishosting.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/krishosting" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/c/Yan110" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/krishostibg" //bebas
-global.email = "krishosting569@gmail.com" //bebas
+global.email = "ryancrisna@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6282180625481'] //premium user
-global.owner = ["6282180625481","628820.7324217"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6282180625481" // nomor wa kalian
-global.ownernomerr = "6282180625481" //nmr wa kalian
+global.premium = ['6281252961511'] //premium user
+global.owner = ["6281252961511","628820.7324217"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281252961511" // nomor wa kalian
+global.ownernomerr = "6281252961511" //nmr wa kalian
 global.thumbnail = "./image/zak.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Z4K' //sticker wm ubah
-global.author = 'Bot Z4K' //sticker wm ganti nama kalian
+global.packname = '© Rimuru' //sticker wm ubah
+global.author = 'Bot Rimuru' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
